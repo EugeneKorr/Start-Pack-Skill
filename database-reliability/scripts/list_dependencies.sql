76a5731baf3cc77a26715f4b72467486604faccf
Volight-14 <vladimir.alushkin68@gmail.com>
2026-03-13 20:28:23 +0300

    Add database-reliability skill

---
+++ database-reliability/scripts/list_dependencies.sql
@@ -0,0 +1,11 @@
+-- Скрипт для поиска зависимостей объекта
+-- Использование: p_object_name - название вьюшки или функции
+SELECT 
+    referencing_entity_name = obj_referencing.relname,
+    referencing_type = obj_referencing.relkind,
+    referenced_entity_name = obj_referenced.relname
+FROM pg_depend 
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+JOIN pg_class obj_referencing ON pg_rewrite.ev_class = obj_referencing.oid
+JOIN pg_class obj_referenced ON pg_depend.refobjid = obj_referenced.oid
+WHERE obj_referenced.relname = 'view_master_transactions'; -- Замените на нужный объект
